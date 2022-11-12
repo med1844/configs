b0a5d175ce3aef5d8ad25a536d7ea49d772443dd
med <medioqrity@gmail.com>
2022-11-11 22:01:45 -0500

    add automated setup script

--- init.sh
+++ init.sh
@@ -1,1 +1,12 @@
+curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+rm get-pip.py
+python3 -m pip install powerline-status
+
+sudo apt install zsh
 curl -sS https://starship.rs/install.sh | sh
+
+sudo apt install tmux
+sudo apt install tree
+sudo apt install tldr
+
