7d2dcd6ca5e0a7d41c7317f61f1d4908ac1439dd
med <medioqrity@gmail.com>
2023-04-04 23:11:18 -0400

    migrate to antigen

--- init.sh
+++ init.sh
@@ -2,14 +2,19 @@ curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 rm get-pip.py
 python3 -m pip install powerline-status
+python3 -m pip install thefuck
 
 powerline_path=$(pip show powerline-status | grep Location: | sed 's/Location: //')
 cp default.json "$powerline_path/powelrine/config_files/themes/tmux"
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+mkdir ~/.zsh
+
 sudo apt update
 sudo apt install zsh
+source ~/.zsh/antigen.zsh
+
 curl -sS https://starship.rs/install.sh | sh
 
 sudo apt install tmux
