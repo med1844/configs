17d6059ebc2310ad09a8b36e0fc81b5b7b3018d9
med <medioqrity@gmail.com>
2023-06-18 01:40:01 -0400

    move astronvim config into this repo & add symlink creation instruction in init.sh

--- init.sh
+++ init.sh
@@ -1,23 +1,18 @@
-curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
-rm get-pip.py
-python3 -m pip install powerline-status
-python3 -m pip install thefuck
-
-powerline_path=$(pip show powerline-status | grep Location: | sed 's/Location: //')
-cp default.json "$powerline_path/powelrine/config_files/themes/tmux"
-
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-mkdir ~/.zsh
-
+# install shell & editor
+sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
-sudo apt install zsh
-curl -L git.io/antigen > ~/.zsh/antigen.zsh
-source ~/.zsh/antigen.zsh
+sudo apt install fish
+sudo apt install neovim
 
+# setup prompt (starship)
 curl -sS https://starship.rs/install.sh | sh
+echo "starship init fish | source" >> ~/.config/fish/config.fish
 
+# setup neovim configuration
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+ln -s $(pwd)/astronvim/user ~/.config/nvim/lua/user  # symlink everything in the repository to the target folder
+
+# install libraries that will be used during pyenv installing python from source
 sudo apt install tmux tree tldr build-essential unzip ripgrep \
   libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl \
   libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
@@ -27,5 +22,11 @@ git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 cd ~/.pyenv && src/configure && make -C src
 cd ~
 
+# setup pip
+curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+rm get-pip.py
+
+# copy all config files to HOME
 cp .* ~
 
